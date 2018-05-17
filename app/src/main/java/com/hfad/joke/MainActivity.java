c7ec1fff25940ce773fea3cad850385e063138b5
deanmock <opworld10@gmail.com>
2018-05-17 13:29:46 -0400

    Initial commit

---
+++ app/src/main/java/com/hfad/joke/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hfad.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class DelayedMessageService extends IntentService {
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Code to do something
+    }
+}
