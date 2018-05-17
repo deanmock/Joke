241ebe294bcd1f9cc33c4c3a2aeb978bac86def7
deanmock <opworld10@gmail.com>
2018-05-17 14:17:32 -0400

    initial commit

--- app/src/main/java/com/hfad/joke/MainActivity.java
+++ app/src/main/java/com/hfad/joke/MainActivity.java
@@ -1,15 +1,22 @@
 package com.hfad.joke;
 
-import android.app.IntentService;
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public class DelayedMessageService extends IntentService {
-    public DelayedMessageService() {
-        super("DelayedMessageService");
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
     }
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        //Code to do something
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.button_response));
+        startService(intent);
     }
 }
